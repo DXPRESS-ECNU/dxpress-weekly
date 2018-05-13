1ecf00b60ca03577ff6f1ca4e18d40d8066de51a
qcmiao1998 <qcmiao@hotmail.com>
2018-05-13 16:57:36 +0800

    Finish Get AK

---
+++ DXPressWeekly/Restapi.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DXPressWeekly
+{
+    class Restapi
+    {
+        /// <summary>
+        /// The function to process GET
+        /// </summary>
+        /// <param name="url">url</param>
+        /// <param name="postDataStr">Back data</param>
+        /// <returns></returns>
+        public static string HttpGet(string url, string postDataStr)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url + (postDataStr == "" ? "" : "?") + postDataStr);
+            request.Method = "GET";
+            request.ContentType = "text/html;charset=UTF-8";
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream myResponseStream = response.GetResponseStream();
+            StreamReader myStreamReader = new StreamReader(myResponseStream);
+            string retString = myStreamReader.ReadToEnd();
+            myStreamReader.Close();
+            myResponseStream.Close();
+
+            return retString;
+        }
+    }
+}
